3b834890173eead0e03f52d76ed5469d7382ca9f
Dnyanesh <dshrirame8@gmail.com>
2020-11-11 15:30:34 +0530

    3rd change in static_root setting.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y5@csa5k-o7)=bb%byd5sjttqfwmmr-g6li_n+50ub1oufbvg^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["18.224.214.1"]
 
