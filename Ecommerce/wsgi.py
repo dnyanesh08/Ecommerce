e3c4bdb3632c027b2c641ec2735de41a9189e86f
Dnyanesh <dshrirame8@gmail.com>
2020-11-14 15:48:22 +0530

    change in wsgi.py file

--- Ecommerce/wsgi.py
+++ Ecommerce/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ecommerce.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ecommerce.settings")
+
 application = get_wsgi_application()
