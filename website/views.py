3fa52b7928e16b26df912c4ff5a1bf20a2cad4e5
Dnyanesh <dshrirame8@gmail.com>
2021-03-03 15:29:16 +0530

    order module changes work

--- website/views.py
+++ website/views.py
@@ -85,6 +85,7 @@ def order(request):
     order_data = Order(amount=amount, name_on_plate=name_on_plate, frame=frame, first_name=first_name, last_name=last_name, email=email, mobile=mobile, address=address, town=town, city=city, state=state, zip_code=zip_code, transaction_no=transaction_no)
     order_data.save()
 
+
     messages.success(request, 'Your Order Placed Successfully...! Thanks For Order...!')
     return redirect("/")
     # messages.success("Your Order Placed Successfully....!")
