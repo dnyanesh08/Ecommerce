7d7a35db0143754aac4f6baea37e1307763e632e
Dnyanesh <dshrirame8@gmail.com>
2021-06-03 00:17:45 +0530

    use email for login remove username

--- accounts/views.py
+++ accounts/views.py
@@ -7,10 +7,10 @@ from django.contrib import messages
 def login(request):
 
     if request.method == 'POST':
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
 
-        user = auth.authenticate(username=username, password=password)
+        user = auth.authenticate(email=email, password=password)
 
         if user is not None:
             auth.login(request, user)
